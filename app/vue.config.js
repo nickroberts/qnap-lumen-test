07abb9e6a8d7c440c2a543e6243a2653e232512f
Nick Roberts <djparadyme@gmail.com>
2020-05-01 21:17:10 -0400

    Initial commit

---
+++ app/vue.config.js
@@ -0,0 +1,21 @@
+const { resolve } = require("path");
+
+module.exports = {
+  chainWebpack: (config) => {
+    config.plugin("html").tap((args) => {
+      args[0].title = "STORJ | Storage Node";
+      return args;
+    });
+  },
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000/",
+        ws: true,
+        changeOrigin: true,
+        pathRewrite: { "^/api": "" },
+      },
+    },
+  },
+  outputDir: resolve(__dirname, "../shared/app"),
+};
---
+++ shared/initpackage.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+CONF=/etc/config/qpkg.conf
+QPKG_NAME="UTLABS"
+LOGFILE=/var/log/${QPKG_NAME}
+QPKG_ROOT=`/sbin/getcfg $QPKG_NAME Install_Path -f ${CONF}`
+APACHE_ROOT=`/sbin/getcfg SHARE_DEF defWeb -d Qweb -f /etc/config/def_share.info`
+
+case "$1" in
+    start)
+        echo "starting package..."
+        # re-integration stuff goes here - symlinks, file copies, etc...
+        ln -s $QPKG_ROOT/app /home/Qhttpd/Web/UTLABS
+        ln -s $QPKG_ROOT/api/public /home/Qhttpd/Web/UTLABS/api
+        ;;
+    stop)
+        echo "stopping package..."
+        # anything required to stop this app running, and reverse re-integration should go here.
+        rm /home/Qhttpd/Web/UTLABS
+        ;;
+    restart)
+        $0 stop
+        $0 start
+        ;;
+    *)
+        echo "run as: $0 {start|stop|restart}"
+        ;;
+esac
